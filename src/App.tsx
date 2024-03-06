39e8ffa40a860b5e2a212dc92e919b9b7c4532a6
Ehab Hussein <ehab.hussein@sap.com>
2024-03-06 14:38:12 +0100

    Adjusted instructions

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalApp from './GlobalApp';
 function App() {
   return (
     // TODO BrowserRouter provider must be added to this component
+    // Providers and contexts must be site in the GlobalApp component
     <BrowserRouter>
       <GlobalApp appPath='' />
     </BrowserRouter>
